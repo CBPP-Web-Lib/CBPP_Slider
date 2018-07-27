3b878638e796b2f02c4d62a9ce8a9777433b746d
nkasprak <nkasprak@cbpp.org>
2018-07-27 12:33:58 -0400

    just download this directly now

--- cbpp_slider.js
+++ cbpp_slider.js
@@ -2,10 +2,10 @@
 module.exports = function($) {
     "use strict";
     var CBPP_Slider = {};
-    require("./node_modules/jquery-ui-dist/jquery-ui_commonJS.js")($);
+    require("./jquery-ui-1.12.1.custom/jquery-ui.min_commonJS.js")($);
     require("./node_modules/jquery-ui-touch-punch/jquery.ui.touch-punch_commonJS.js")($);
     require("./cbpp_slider.css");
-    require("jquery-ui/themes/base/base.css");
+    require("./jquery-ui-1.12.1.custom/jquery-ui.min.css");
     CBPP_Slider.Slider = function(selector, options) {
         if (typeof(options)==="undefined") {
             options = {};
--- commonJS-jquery-ui.js
+++ commonJS-jquery-ui.js
@@ -3,20 +3,52 @@ function fixFile(path, $) {
   var file = fs.readFileSync(path);
   fs.writeFileSync(path.replace(".js","") + "_commonJS.js", "module.exports=function(" + $ + ") {" + file + "};");
 }
-fixFile("./node_modules/jquery-ui-dist/jquery-ui.js", "jQuery");
-fixFile("./node_modules/jquery-ui-touch-punch/jquery.ui.touch-punch.js", "jQuery");
 
-/*TO DO: figure out how to do a post request in node
+/*get jquery ui*/
 
-version: 1.12.1
-widget: on
-keycode: on
-widgets/mouse: on
-widgets/slider: on
-theme: none
-theme-folder-name: no-theme
-scope: 
+const https = require("https");
+const querystring = require("querystring");
+const unzip = require("extract-zip");
+var jquery_ui_write_stream = fs.createWriteStream(__dirname + "/jquery-ui.zip");
+var postData = querystring.stringify({
+  "version": "1.12.1",
+  "widget": "on",
+  "keycode": "on",
+  "widgets/mouse": "on",
+  "widgets/slider": "on",
+  "theme": "none",
+  "theme-folder-name": "no-theme"
+});
+var jquery_ui_req = https.request({
+  host: 'download.jqueryui.com',
+  port: 443,
+  method: 'POST',
+  path: '/download',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': postData.length
+  }
+}, (res) => {
+  res.on("data", (d) => {
+    jquery_ui_write_stream.write(d);
+  });
+  res.on("end", () => {
+    unzip(__dirname + "/jquery-ui.zip", {dir: __dirname}, (err) => {
+      fixFile(__dirname + "/jquery-ui-1.12.1.custom/jquery-ui.min.js","jQuery");
+    });
+  });
+});
+jquery_ui_req.write(postData);
+jquery_ui_req.end();
 
-https://download.jqueryui.com/download
+/*get touch punch*/
+var touch_punch_stream = fs.createWriteStream(__dirname + "/jquery-ui.touch_punch.min.js"); 
+var touch_punch_req = https.get("https://raw.githubusercontent.com/furf/jquery-ui-touch-punch/master/jquery.ui.touch-punch.min.js", (res) => {
+  res.on("data", (d) => {
+    touch_punch_stream.write(d);
+  });
+  res.on("end", () => {
+    fixFile(__dirname + "/jquery-ui.touch_punch.min.js", "jQuery");
+  });
+});
 
-*/
